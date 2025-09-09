e32b30ee82743e61c19940dfa5b37b577c04fa43
codebyaman <codedbyaman09@gmail.com>
2025-09-10 00:01:17 +0530

    Add files via upload

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+git remote remove origin
+git remote add origin https://github.com/codedbyaman09-coder/hotel-booking-system.git
+
